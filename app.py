2cf227a16e31f307fa23ff92a2f6d74f209d2d93
Asher <asherdiamant@users.noreply.github.com>
2019-09-09 11:02:52 -0400

    Cleaned up unneeded templates and improved remaining template

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from keywords import get_suggestions
 # create the application object
 app = Flask(__name__)
 
+
 # use decorators to link the function to a url
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -12,10 +13,8 @@ def home():
         suggestion_list = get_suggestions(request.form['queryroot'])
         return render_template('keywords.html', suggestion_list=suggestion_list)
     return render_template('keywords.html')
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')  # render a template
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
